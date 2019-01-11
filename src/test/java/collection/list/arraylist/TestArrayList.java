326780080b5478fc59739669b21def1da9be3b55
neo <neo_fun@163.com>
2019-01-11 11:06:38 +0800

    rm iml again

---
+++ src/test/java/collection/list/arraylist/TestArrayList.java
@@ -0,0 +1,10 @@
+package collection.list.arraylist;
+
+/**
+ * 测试ArrayList
+ *
+ * @author liaofeng
+ * 2019/1/10 7:28 PM
+ */
+public class TestArrayList {
+}
---
+++ src/test/java/collection/list/linkedlist/TestLinkedList.java
@@ -0,0 +1,10 @@
+package collection.list.linkedlist;
+
+/**
+ * 测试LinkedList
+ *
+ * @author liaofeng
+ * 2019/1/11 10:53 AM
+ */
+public class TestLinkedList {
+}
